4243a6a45080fdcf5378cff3dcbf5d7248b3c58b
Ishank <ishankkumar123@gmail.com>
2023-05-11 15:31:13 +0530

    "add rename.sh"

--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@ Roll no. - CS22B1043
 added deleteAllTxtFiles.sh
 added cowsay.sh
 added triangle.sh
+added rename.sh
---
+++ rename.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+find -name "*.txt" > temp    #finds all the .txt files in the current and subdirectories and the optput is passed to temp
+
+file=temp  #declaring file variable
+
+i=0
+while read -r line;        #reads line by line and stores it to 'line'
+do
+    mv $line file$i.txt    #renames with the format file0, file1, ...
+    ((i++))
+done < "$file"             #reading from the file stored in 'file' variable
+
+rm temp                    #removing temp file after using
